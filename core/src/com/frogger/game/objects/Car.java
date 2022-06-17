2d34a6942be8ed13b37b1c7b54e0ac1226284592
vadimpk <vadyman.pk@gmail.com>
2022-06-17 19:41:29 +0300

    added textures for cars

--- core/src/com/frogger/game/objects/Car.java
+++ core/src/com/frogger/game/objects/Car.java
@@ -3,40 +3,67 @@ package com.frogger.game.objects;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.graphics.g2d.TextureRegion;
 import com.frogger.game.Util;
+
+import java.util.Random;
 
 public class Car extends MovingObject {
 
-    private static final Texture DEFAULT_CAR_TEXTURE = new Texture(Gdx.files.internal("tile2.png"));
+    private static final Texture CAR_PACK = new Texture(Gdx.files.internal("objects/car/cars.png"));
+
+    private static final TextureRegion CAR_HEAD_TEXTURE_1 = new TextureRegion(CAR_PACK, 0, 0, 60, 75);
+    private static final TextureRegion CAR_TAIL_TEXTURE_1 = new TextureRegion(CAR_PACK, 0, 75, 60, 75);
+    private static final TextureRegion CAR_HEAD_TEXTURE_2 = new TextureRegion(CAR_PACK, 0, 150, 60, 75);
+    private static final TextureRegion CAR_TAIL_TEXTURE_2 = new TextureRegion(CAR_PACK, 0, 225, 60, 75);
+    private static final TextureRegion CAR_HEAD_TEXTURE_3 = new TextureRegion(CAR_PACK, 0, 300, 60, 75);
+    private static final TextureRegion CAR_TAIL_TEXTURE_3 = new TextureRegion(CAR_PACK, 0, 375, 60, 75);
+
+
     private static final boolean SAFE = false;
 
-    private Texture texture;
+    private TextureRegion textureHead;
+    private TextureRegion textureTail;
+    private int textureRotation;
 
     public Car(float size, float x, float y, float speed, int length, Util.Direction direction) {
         super(size, x, y, speed, length, direction);
 
         this.setSafe(SAFE);
-        this.texture = DEFAULT_CAR_TEXTURE;
+        Random rand = new Random();
+        int t = rand.nextInt(3);
+        if (t == 0) {
+            textureHead = CAR_HEAD_TEXTURE_1;
+            textureTail = CAR_TAIL_TEXTURE_1;
+        }
+        if (t == 1) {
+            textureHead = CAR_HEAD_TEXTURE_2;
+            textureTail = CAR_TAIL_TEXTURE_2;
+        }
+        if (t == 2) {
+            textureHead = CAR_HEAD_TEXTURE_3;
+            textureTail = CAR_TAIL_TEXTURE_3;
+        }
+
+        if (direction == Util.Direction.LEFT) textureRotation = 0;
+        else textureRotation = 180;
     }
 
     @Override
     public void render(SpriteBatch batch) {
         move();
-        for (int i = 0; i < getLength(); i++) {
-            batch.draw(texture, getX() + i * getSize(), getY(), getSize(), getSize());
+        if (getDirection() == Util.Direction.LEFT) {
+            batch.draw(textureHead, getX(), getY(), getSize()/2, getSize()/2, getSize(), getSize(),1,1, textureRotation);
+            batch.draw(textureTail, getX()+ getSize(), getY(), getSize()/2, getSize()/2, getSize(), getSize(),1,1, textureRotation);
+        } else {
+            batch.draw(textureTail, getX(), getY(), getSize()/2, getSize()/2, getSize(), getSize(),1,1, textureRotation);
+            batch.draw(textureHead, getX()+ getSize(), getY(), getSize()/2, getSize()/2, getSize(), getSize(),1,1, textureRotation);
+
         }
     }
 
 
     public static void dispose() {
-        DEFAULT_CAR_TEXTURE.dispose();
-    }
-
-    public void setTexture(Texture texture) {
-        this.texture = texture;
-    }
-
-    public Texture getTexture() {
-        return texture;
+        CAR_PACK.dispose();
     }
 }
